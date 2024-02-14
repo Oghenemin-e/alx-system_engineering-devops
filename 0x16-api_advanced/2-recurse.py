845687ad816c47b6067381f94ee18146ac191c58
Oghenemin-e <lawalogheneminebabatunde@gmail.com>
2024-02-14 14:55:48 +0000

    0x16. API advanced

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,5 +1,7 @@
+#!/usr/bin/python3
+"""recursive function that queries the Reddit API and returns
+ a list containing the titles of all hot articles for a given subreddit. """
 
-n to query a list of all hot posts on a given Reddit subreddit."""
 import requests
 
 
@@ -7,7 +9,7 @@ def recurse(subreddit, hot_list=[], after="", count=0):
     """Returns a list of titles of all hot posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "underscoDe@alx-holbertonschool"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     params = {
         "after": after,
