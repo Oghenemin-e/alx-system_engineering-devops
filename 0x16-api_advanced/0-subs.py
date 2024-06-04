9c9a5f84f32b87819fd6afde7c067dfea9c48e63
Oghenemin_e <lawalogheneminebabatunde@gmail.com>
2024-06-04 07:05:44 -0700

    How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,34 @@
 #!/usr/bin/python3
-# get subs
-from requests import get
-from sys import argv
+"""
+This module defines a function to get the number of subscribers for a given subreddit
+using the Reddit API.
+"""
+
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """subs"""
-    head = {'User-Agent': 'Dan Kazam'}
-    count = get('https://www.reddit.com/r/{}/about.json'.format(
-        subreddit), headers=head).json()
-    try:
-        return count.get('data').get('subscribers')
-    except:
-        return 0
+    """
+    Queries the Reddit API and returns the number of subscribers for a given subreddit.
+    If an invalid subreddit is given, returns 0.
 
-if __name__ == "__main__":
-    number_of_subscribers(argv[1])
+    Args:
+        subreddit (str): The name of the subreddit to query.
+
+    Returns:
+        int: Number of subscribers or 0 if the subreddit is invalid.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'custom-user-agent'}
+    
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data.get('data', {}).get('subscribers', 0)
+        else:
+            return 0
+    except requests.RequestException:
+        return 0
+    except ValueError:
+        return 0
